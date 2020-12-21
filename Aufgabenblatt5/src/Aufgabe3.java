9feeb7bb566276d3c7983f7d8b80b09f9e509aa7
MarvinAsmen <marvin.arduc@gmail.com>
2020-12-21 19:09:32 +0100

    ❄️  Aufgabenblatt 5, Task 3 done

--- Aufgabenblatt5/src/Aufgabe3.java
+++ Aufgabenblatt5/src/Aufgabe3.java
@@ -62,8 +62,59 @@ public class Aufgabe3 {
 
     //detect waldo by template matching and return its bounding box values
     private static int[] detectWaldo(int[][] imgArrayGrayscale,int[][] templateArray) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+
+        int[] boundingBox = new int[4]; // {x1,y1,x2,y2}, (x1,y1) top left, (x2,y2) bottom right
+
+        int gray_width = imgArrayGrayscale[0].length;
+        int gray_height = imgArrayGrayscale.length;
+
+        // Centerpoint of templateArray
+        int temp_width = templateArray[0].length;
+        int temp_height = templateArray.length;
+        int hebm = temp_width/2; // - horizontal elements beside middle
+        int vebm = temp_height/2; // - vertical elements beside middle
+
+        int lowest_sad = -1;
+        int wcpx = 0; // -Waldos Center Point X-Coordinate
+        int wcpy = 0; // -Waldos Center Point Y-Coordinate
+
+        // Run Through every Element and Calculate Sum of Absolute Difference
+        for (int columns = 0; columns < gray_height; columns++) {
+            for (int rows = 0; rows < gray_width; rows++) {
+
+                // Check if Temp is not outside
+                if(
+                        (rows-hebm) >= 0 && // - Not Outside Left Side
+                        (rows+hebm) < gray_width && // - Not Outside Right Side
+                        (columns-vebm) >= 0 && // - Not Outside Bottom Side
+                        (columns+vebm) < gray_height // - Not Outside Top Side
+                ){
+                    int sad = 0;
+
+                    // Runs Through the Selected Area of imgArrayGrayscale and templateArray at the Same Time
+                    for (int tc = 0, sc = columns-vebm; tc < temp_height; tc++, sc++) { // - Selected Column, Template Column
+                        for (int tr = 0, sr = rows-hebm; tr < temp_width; tr++, sr++) { // - Selected Row, Template Row
+                            sad+= Math.abs(imgArrayGrayscale[sc][sr]-templateArray[tc][tr]);
+                        }
+                    }
+                    if(lowest_sad == -1 || sad<lowest_sad){
+                        lowest_sad = sad;
+                        wcpy = columns;
+                        wcpx = rows;
+                    }
+                }
+            }
+        }
+
+
+        // Set boundingBox values
+        // {x1,y1,x2,y2}, (x1,y1) top left, (x2,y2) bottom right
+        boundingBox[0] = wcpx - hebm; // x top left
+        boundingBox[1] = wcpy - vebm; // y top left
+        boundingBox[2] = wcpx + hebm; // x bottom right
+        boundingBox[3] = wcpy + vebm; // y bottom right
+
+        return boundingBox;
     }
 
     public static void main(String[] args) {
@@ -119,7 +170,22 @@ public class Aufgabe3 {
         int[] boundingBox = detectWaldo(imgArrayGrayscale,templateArray);
 
         if (boundingBox !=null) {
-            //TODO: heben Sie die gefundene Waldo Bounding Box im Bild hervor, indem Sie den Rest des Bildes verdunkeln
+            // Run Through Every Element
+            for (int columns = 0; columns < imgArrayR.length; columns++) {
+                for (int rows = 0; rows < imgArrayR[0].length; rows++) {
+
+                    // If not inside the field then make darker
+                    if(
+                            !((rows >= boundingBox[0] && rows <= boundingBox[2]) &&
+                            (columns >= boundingBox[1] && columns <= boundingBox[3]))
+                    ){
+                        // If below 0, set to 0, else set to element - 150
+                        imgArrayR[columns][rows] = Math.max(0,(imgArrayR[columns][rows]-150));
+                        imgArrayB[columns][rows] = Math.max(0,(imgArrayB[columns][rows]-150));
+                        imgArrayG[columns][rows] = Math.max(0,(imgArrayG[columns][rows]-150));
+                    }
+                }
+            }
         }
 
         drawImage(imgArrayR,imgArrayG,imgArrayB);
@@ -131,3 +197,79 @@ public class Aufgabe3 {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Made by
+ __  __                  _                                                              _
+|  \/  |                (_)           /\                                  /\           | |
+| \  / | __ _ _ ____   ___ _ __      /  \   ___ _ __ ___   ___ _ __      /  \   _ __ __| |_   _  ___
+| |\/| |/ _` | '__\ \ / / | '_ \    / /\ \ / __| '_ ` _ \ / _ \ '_ \    / /\ \ | '__/ _` | | | |/ __|
+| |  | | (_| | |   \ V /| | | | |  / ____ \\__ \ | | | | |  __/ | | |  / ____ \| | | (_| | |_| | (__
+|_|  |_|\__,_|_|    \_/ |_|_| |_| /_/    \_\___/_| |_| |_|\___|_| |_| /_/    \_\_|  \__,_|\__,_|\___|
+12024700
+Mon 21. Dec 2020
+
+ */
